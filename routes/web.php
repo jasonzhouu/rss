8ec492a2269a17cdd6dda3a9d6e727183697bfe6
周宇盛 Jason Zhou <jasonzhouu@gmail.com>
2018-04-15 21:40:55 +0800

    确定好 router

--- routes/web.php
+++ routes/web.php
@@ -11,11 +11,10 @@
 |
 */
 
-Route::get('/article_list/{page}', 'ArticleController@get_article_list');
-Route::get('/article_detail/{id}', 'ArticleController@get_article_detail');
+// 后端将文章列表分页，前端无论何时获取文章列表时，都要指定是第几页，比如/article_list/{open_id}?page=10
+Route::get('/article_list/{open_id}', 'ArticleController@get_article_list');
 
-Route::post('/reading_history/{id}', 'ArticleController@save_reading_history');
-
-Route::post('/register', 'Auth\RegisterController@register');
-Route::post('/login', 'Auth\LoginController@login');
-Route::get('/logout', 'Auth\LogoutController@logout');
+// 前端每次打开一个页面，同时上传 article_id 和 open_id
+// open_id, article_id 会被存到数据库的 reading_hitory 表
+// 前端的路由就是 /article_detail/***?open_id=***
+Route::get('/article_detail/{article_id}', 'ArticleController@get_article_detail');
