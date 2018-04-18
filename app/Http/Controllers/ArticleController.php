860df719479285bd48fdb4ed9c3c3baaa6cbd066
周宇盛 Jason Zhou <jasonzhouu@gmail.com>
2018-04-18 11:00:17 +0800

    创建 StarController, ArticleController, ReadingHistoryController

---
+++ app/Http/Controllers/ArticleController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ArticleController extends Controller
+{
+    public function get_article_list()
+    {
+
+    }
+
+    public function get_article_detail()
+    {
+        
+    }
+}
