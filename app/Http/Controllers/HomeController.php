0071b24cd8e1ddfefdb0fd0998a9bb23d77b4f4c
周宇盛 Jason Zhou <jasonzhouu@gmail.com>
2018-04-18 10:56:14 +0800

    HomeController@index return index.html

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    //返回主页面
+    public function index()
+    {
+        return 'index';
+    }
+}
