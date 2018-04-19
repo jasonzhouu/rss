d852d8b88b990513655cf5cc0c372a1ecb86fe53
周宇盛 Jason Zhou <jasonzhouu@gmail.com>
2018-04-19 11:11:26 +0800

    article_list 先不管登录的事情

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -3,12 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Article;
 
 class ArticleController extends Controller
 {
     public function get_article_list()
     {
-
+        return Article::paginate(15);
     }
 
     public function get_article_detail()
--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,10 @@
 Route::get('/', 'HomeController@index');
 
 // 后端将文章列表分页，前端无论何时获取文章列表时，都要指定是第几页，比如/article_list/{open_id}?page=10
-Route::get('/article_list/{open_id}', 'ArticleController@get_article_list');
+// Route::get('/article_list/{open_id}', 'ArticleController@get_article_list');
+
+// 先不管登录的事情
+Route::get('/article_list', 'ArticleController@get_article_list');
 
 // 前端每次打开一个页面，同时上传 article_id 和 open_id
 // open_id, article_id 会被存到数据库的 reading_hitory 表
