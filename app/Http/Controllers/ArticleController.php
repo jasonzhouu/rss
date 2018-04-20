b97ce1877f5924a0f4b12811de0e2de7c52d514b
jasonzhouu <jasonzhouu@163.com>
2018-04-20 10:07:42 +0800

    save click history

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Article;
+use App\ReadingHistoryArticle;
 
 class ArticleController extends Controller
 {
@@ -14,6 +15,10 @@ class ArticleController extends Controller
 
     public function get_article_detail($article_id)
     {
+        $reading_history = new ReadingHistoryArticle;
+        $reading_history->user_id = 0;
+        $reading_history->article_id = $article_id;
+        $reading_history->save();
         return Article::find($article_id);
     }
 }
