fc76fda42508097f01861bc810c7868948e30e5c
周宇盛 Jason Zhou <jasonzhouu@gmail.com>
2018-06-01 06:36:16 +0800

    只展示当天推荐的文章

--- app/Http/Controllers/RecommandArticleController.php
+++ app/Http/Controllers/RecommandArticleController.php
@@ -10,8 +10,10 @@ class RecommandArticleController extends Controller
     // 返回推荐文章列表
     public function get_article_list()
     {
+        $date = date('Y-m-d', time());
         return RecommendArticle::with('article')
-                                ->paginate(15);
+        ->whereDate('created_at', $date)
+        ->paginate(15);
         // @TODO 筛选出当天的文章
     }
 }
